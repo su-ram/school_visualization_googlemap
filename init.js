17df808760fcd8976cbc2411ad7f896ea852a080
su-ram <swamys@naver.com>
2020-12-17 09:34:03 +0900

    파일 업로드 오류 해결
    오류 : 동일한 파일을 재업로드할 때 안 되는 경우를 대비하여 초기화를
    시켰더니 typemismatch 오류 발생
    해결 : 초기화하는 과정에서 change 이벤트가 또 발생했는데 그 과정에서
    handle_fr 메소드가 또 실행됨. 그래서 file 길이가 0일 경우 (= 초기화)에는
    바로 리턴하도록 수정. ^o^

--- init.js
+++ init.js
@@ -130,8 +130,7 @@ var initGridData = new Array();
 var locations = [{ lat: -31.56391, lng: 147.154312}, { lat: -33.718234, lng: 150.363181 }, { lat: -33.727111, lng: 150.371124 }, { lat: -33.848588, lng: 151.209834 }, { lat: -33.851702, lng: 151.216968 }, { lat: -34.671264, lng: 150.863657 }, { lat: -35.304724, lng: 148.662905 }, { lat: -36.817685, lng: 175.699196 }, { lat: -36.828611, lng: 175.790222 }, { lat: -37.75, lng: 145.116667 }, { lat: -37.759859, lng: 145.128708 }, { lat: -37.765015, lng: 145.133858 }, { lat: -37.770104, lng: 145.143299 }, { lat: -37.7737, lng: 145.145187 }, { lat: -37.774785, lng: 145.137978 }, { lat: -37.819616, lng: 144.968119 }, { lat: -38.330766, lng: 144.695692 }, { lat: -39.927193, lng: 175.053218 }, { lat: -41.330162, lng: 174.865694 }, { lat: -42.734358, lng: 147.439506 }, { lat: -42.734358, lng: 147.501315 }, { lat: -42.735258, lng: 147.438 }, { lat: -43.999792, lng: 170.463352 }];
 
 window.onload = function () {
-  console.log("call type............... 0");
-
+ 
   initMap();
   initGrid();
   initColorPicker();
@@ -183,7 +182,7 @@ function deleteAllRows(){
   mygrid.clear();
   mygrid.resetData(createInitRows(0));
   deleteMarkers();
-
+  $('#excelFileName').text('');
 }
 
 
@@ -462,10 +461,16 @@ function handleFile(e) {
 
 function handle_fr(e) {
 
-  console.log(e.target.files);
-  var files = e.target.files,
-      f = files[0];
+  var files = e.target.files;
 
+ 
+  if(files.length == 0){
+    return;
+  }
+
+  var f = files[0];
+
+  
   $('#excelFileName').text(f.name);
   
   var reader = new FileReader();
@@ -505,10 +510,11 @@ function handle_fr(e) {
     });
   };
   if (rABS) {
-    console.log("rABS");
+    
     reader.readAsBinaryString(f);
-  }else {console.log("not rABS"); 
-  reader.readAsArrayBuffer(f);}
+  }else { 
+  reader.readAsArrayBuffer(f);
+}
 
   document.getElementById('excelFile').value='';
 
